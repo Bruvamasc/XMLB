155b215d6fbd4d65a9cec4fa4c8faa71fb8d1a34
Bruvamasc <bruvamasc@gmail.com>
2022-11-07 18:46:59 +0200

    FIX docs 5

--- docs/menudata.js
+++ docs/menudata.js
@@ -25,7 +25,8 @@
 var menudata={children:[
 {text:"Титульная страница",url:"index.html"},
 {text:"Описания",url:"pages.html"},
-{text:"Группы",url:"modules.html"},
+{text:"Основные инсрументы",url:"group__general.html"},
+{text:"Второстепенные инсрументы",url:"group__secondary.html"},
 {text:"Классы",url:"annotated.html",children:[
 {text:"Классы",url:"annotated.html"},
 {text:"Алфавитный указатель классов",url:"classes.html"},
