7dfe359898ad46cb81a1cbf9a74e252da9b1518e
Bruvamasc <bruvamasc@gmail.com>
2022-11-07 19:00:44 +0200

    Empty

--- docs/menudata.js
+++ docs/menudata.js
@@ -22,7 +22,6 @@
 
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
-
 var menudata={children:[
 {text:"Титульная страница",url:"index.html"},
 {text:"Описания",url:"pages.html"},
